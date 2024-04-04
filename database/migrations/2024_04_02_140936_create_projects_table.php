eef77e450a42172775edc506dd1217733ea4311f
FrancescoCesari97 <freedus97@gmail.com>
2024-04-04 17:26:45 +0200

    aggiunta il foreignId nella migration del project poi dopo un reset cambiato data alla migration Types cosi da farla eseguire prima dei project ed avere il foreignId

--- database/migrations/2024_04_02_140936_create_projects_table.php
+++ database/migrations/2024_04_02_140936_create_projects_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('type_id')->constrained();
             $table->string('title', 150);
             $table->string('slug', 150);
             $table->text('content');
